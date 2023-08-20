48e9be3ea6f5f468f0a87a24b476035d15df4008
Subulzaidi <subul9010@gmail.com>
2023-08-20 18:52:25 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Heading from "./App/heading";
 
-function App() {
+const data = [
+  {
+    name: "hadi",
+    age: "23",
+    status: "developer",
+  },
+  {
+    name: "Wali",
+    age: "23",
+    status: "Python Developer",
+  },
+  {
+    name: "Sherazi",
+    age: "20",
+    status: "AI Developer",
+  },
+];
+
+const App = () => {
+  const [list, setlist] = useState(data);
+
+  const [name, setName] = useState("");
+  const [status, setStatus] = useState("");
+  const [age, setAge] = useState(0);
+
+  const addElementInListArr = () => {
+    const newPerson = {
+      name,
+      age,
+      status,
+    };
+
+    setlist([...list, newPerson]);
+  };
+  const NewName = (inputProp) => {
+    setName(inputProp.target.value);
+  };
+  const Newage = (inputProp) => {
+    setAge(inputProp.target.value);
+  };
+  const NewStatus = (inputProp) => {
+    setStatus(inputProp.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Heading />
+      <div className="data">
+        <input
+          className="box inputs"
+          placeholder="Enter the Name 🤌"
+          onChange={NewName}
+          value={name}
+        />
+        <input
+          className="box inputs"
+          placeholder="Enter the Age 🤌"
+          onChange={Newage}
+        />
+        <input
+          className="box inputs"
+          placeholder="Enter the Status 🤌"
+          onChange={NewStatus}
+          value={status}
+        />
+      </div>
+      <div id="btn">
+      <button className="box btns" onClick={addElementInListArr}>
+        Add ELE
+      </button>
+      </div>
+
+      {list.map((x) => {
+        return (
+          <div className="box">
+            {" "}
+            {x.name} - {x.status} - {x.age}{" "}
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
