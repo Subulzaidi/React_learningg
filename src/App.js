547bdf254c24a9cf44563169e31e8d50b48cc88d
Subulzaidi <subul9010@gmail.com>
2023-08-24 17:41:07 +0500

    Task 2 completed!!

--- src/App.js
+++ src/App.js
@@ -2,39 +2,42 @@ import React, { useState } from "react";
 import Heading from "./App/heading";
 
 const data = [
-  {
-    name: "hadi",
-    age: "23",
-    status: "developer",
-  },
-  {
-    name: "Wali",
-    age: "23",
-    status: "Python Developer",
-  },
-  {
-    name: "Sherazi",
-    age: "20",
-    status: "AI Developer",
-  },
+  // { id: 1, name: "hadi", age: "23", status: "developer" },
+  "+",
 ];
-
+const initialVal = {
+  name: "",
+  status: "",
+  age: "",
+};
 const App = () => {
   const [list, setlist] = useState(data);
-
   const [name, setName] = useState("");
   const [status, setStatus] = useState("");
-  const [age, setAge] = useState(0);
+  const [age, setAge] = useState("");
 
   const addElementInListArr = () => {
-    const newPerson = {
-      name,
-      age,
-      status,
-    };
+    if (name === "" || age === 0 || status === "") {
+      alert("You forget to fill all the fields!!!");
+    } else {
+      const newPerson = {
+        id: new Date(),
+        name,
+        age,
+        status,
+      };
 
-    setlist([...list, newPerson]);
+      setlist([...list, newPerson]);
+      setAge(initialVal.age);
+      setName(initialVal.name);
+      setStatus(initialVal.status);
+    }
   };
+  const deleteElement = (id) => {
+    const updatedList = list.filter((x) => x.id !== id);
+    setlist(updatedList);
+  };
+
   const NewName = (inputProp) => {
     setName(inputProp.target.value);
   };
@@ -44,44 +47,59 @@ const App = () => {
   const NewStatus = (inputProp) => {
     setStatus(inputProp.target.value);
   };
-  return (
-    <div className="container">
-      <Heading />
-      <div className="data">
-        <input
-          className="box inputs"
-          placeholder="Enter the Name 🤌"
-          onChange={NewName}
-          value={name}
-        />
-        <input
-          className="box inputs"
-          placeholder="Enter the Age 🤌"
-          onChange={Newage}
-        />
-        <input
-          className="box inputs"
-          placeholder="Enter the Status 🤌"
-          onChange={NewStatus}
-          value={status}
-        />
-      </div>
-      <div id="btn">
-      <button className="box btns" onClick={addElementInListArr}>
-        Add ELE
-      </button>
-      </div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    return (
+      <div className="container">
+        <Heading />
+        <form type="submit" onSubmit={handleSubmit} className="data">
+          <input
+            className="box inputs"
+            placeholder="Enter the Name 🤌"
+            onChange={NewName}
+            value={name}
+          />
+          <input
+            type="number"
+            className="box inputs"
+            placeholder="Enter the Age 🤌"
+            onChange={Newage}
+            value={age}
+          />
+          <input
+            className="box inputs"
+            placeholder="Enter the Status 🤌"
+            onChange={NewStatus}
+            value={status}
+          />
+        </form>
+        <div id="btn">
+          <button className="box btns" onClick={addElementInListArr}>
+            Add ELE
+          </button>
+        </div>
 
-      {list.map((x) => {
-        return (
-          <div className="box">
-            {" "}
-            {x.name} - {x.status} - {x.age}{" "}
-          </div>
-        );
-      })}
-    </div>
-  );
+        {list.map((x) => {
+          return (
+            <div className="box">
+              {" "}
+              {x.name} - {x.status} - {x.age}
+              {"   "}
+              <div className="deltebtn">
+                <button
+                  id="deltebtn"
+                  type="remove"
+                  onClick={() => deleteElement(x.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
 };
 
 export default App;
