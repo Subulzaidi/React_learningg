629b1829d93bf34ea0ac861011f9e2ecf6f1f85f
Subulzaidi <subul9010@gmail.com>
2023-09-03 17:47:05 +0500

    Accuweather

--- src/App.js
+++ src/App.js
@@ -1,67 +1,79 @@
-import React, { useState } from "react";
-import Heading from "./App/heading";
-const data = [
-  {
-    id: new Date(),
-    task: "My task",
-  },
-];
-const initalVal = {
-  task: "",
-};
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [arr, setarr] = useState(data);
-  const [task, setTask] = useState("");
-  const addNewTAsk = () => {
-    if (task === "") {
-      alert("Invalid Input!");
+  const [search, setseacrh] = useState("Karachi");
+  const [city, setcity] = useState("");
+  const [temp, settemp] = useState(0);
+  const [humidiy, sethumidiy] = useState(0);
+  const [max, setmax] = useState(0);
+  const [min, setmin] = useState(0);
+
+  const addCity = () => {
+    if (city === "") {
+      alert("Please enter the city name!!!");
     } else {
-      const newData = {
-        id: new Date(),
-        task,
-      };
-      setarr([...arr, newData]);
-      setTask(initalVal.task);
+      setseacrh(city);
+      console.log(city);
     }
   };
-  const NewTask = (inputProp) => {
-    setTask(inputProp.target.value);
+  const newCity = (e) => {
+    setcity(e.target.value);
   };
 
-  const deleteElement = (id) => {
-    let updatearr = arr.filter((x) => x.id !== id);
-    setarr(updatearr);
-  };
+  useEffect(() => {
+    async function weather() {
+      try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=Metric&appid=5dc2998c93279722e6a408b0d8f4d0bf`;
+        const fetchdata = await fetch(url);
+        const res = await fetchdata.json();
+        settemp(res.main.temp);
+        sethumidiy(res.main.humidity);
+        setmax(res.main.temp_max);
+        setmin(res.main.temp_min);
+
+        console.log("responded");
+      } catch {
+        console.log("loading");
+      }
+    }
+    weather();
+  });
+
   return (
-    <div className="container">
-      <Heading />
-      <div className="inputbox">
+    <>
+      <div>
         <input
-          className="IN"
-          placeholder="Add something to do"
-          onChange={NewTask}
-          value={task}
+          type="search"
+          className="search-bar "
+          placeholder="Enter your city.."
+          value={city}
+          onChange={newCity}
         />
-        <button type="submit" onClick={addNewTAsk}>
-          save
+        <button className="searchbar" type="submit" onClick={addCity}>
+          🔍
         </button>
-      </div>
+        <div className="main">
+          <img
+            className="w-img"
+            src="https://o.remove.bg/downloads/dab6e57a-7f25-4c4d-b338-80673c4b0c94/unnamed-removebg-preview.png"
+          />
+          <h1 className="city">
+            {search}
+            <br />
+            <span className="temp">{temp}°C</span>
+          
 
-      {arr.map((x) => {
-        return (
-          <div className="box">
-            <input type="checkbox" />
-            {x.task}
-            <div className="deltebtn">
-              <button type="remove" onClick={() => deleteElement(x.id)}>
-                Remove
-              </button>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+          </h1>
+          <span className="temp">Humidity:{humidiy}</span>
+          <span className="temp"  >
+              temp-min:{min} 
+            </span>
+            <span className="temp"  >
+              temp-max:{max} 
+            </span>
+        </div>
+      </div>
+    </>
   );
 };
 
